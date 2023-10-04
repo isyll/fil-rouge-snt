2b1f1c8472215daf8d28e8371fc85a32aa76dfaa
Ibrahima Sylla <isyll711@gmail.com>
2023-10-04 15:38:42 +0000

    crud etudiant

---
+++ api/src/Entity/Etudiant.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\EtudiantRepository;
+use App\Validator\Constraints\TelephoneFormat;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: EtudiantRepository::class)]
+#[UniqueEntity('email', message: "L'adresse email existe déjà")]
+#[UniqueEntity('telephone', message: "Le numéro de téléphone existe déjà")]
+#[ApiResource()]
+class Etudiant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotNull(message: 'Le prénom est manquant')]
+    #[Groups('write')]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotNull(message: 'Le nom est manquant')]
+    #[Groups('write')]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\NotNull(message: 'La date de naissance est manquante')]
+    #[Groups('write')]
+    private ?\DateTimeInterface $date_naissance = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    #[Assert\NotNull(message: 'Le numéro de téléphone est manquant')]
+    #[TelephoneFormat]
+    #[Groups('write')]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    #[Assert\NotNull(message: "L'adresse email est manquante")]
+    #[Assert\Email(message: "L'adresse email '{{ value }}' n'est pas valide.")]
+    #[Groups('write')]
+    private ?string $email = null;
+
+    #[ORM\OneToMany(targetEntity: Inscription::class, mappedBy: 'classe')]
+    private Collection $classes;
+
+    public function __construct()
+    {
+        $this->classes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $date_naissance): static
+    {
+        $this->date_naissance = $date_naissance;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Classe>
+     */
+    public function getClasses(): Collection
+    {
+        return $this->classes;
+    }
+
+    public function addClass(Classe $class): static
+    {
+        if (!$this->classes->contains($class)) {
+            $this->classes->add($class);
+            $class->addEtudiant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeClass(Classe $class): static
+    {
+        if ($this->classes->removeElement($class)) {
+            $class->removeEtudiant($this);
+        }
+
+        return $this;
+    }
+}
