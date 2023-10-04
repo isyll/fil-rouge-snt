313c455b946c447227739ba34617a03baee3a9bf
Ibrahima Sylla <isyll711@gmail.com>
2023-10-04 12:23:56 +0000

    feat : message de validation

--- api/src/Entity/AnneeScolaire.php
+++ api/src/Entity/AnneeScolaire.php
@@ -7,6 +7,7 @@ use App\Repository\AnneeScolaireRepository;
 use App\Validator\Constraints\AnneeScolaireFormat;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AnneeScolaireRepository::class)]
 #[UniqueEntity('libelle')]
@@ -19,6 +20,7 @@ class AnneeScolaire
     private ?int $id = null;
 
     #[ORM\Column(length: 255, unique: true)]
+    #[Assert\NotNull(message: 'Le libellé est manquant')]
     #[AnneeScolaireFormat]
     private ?string $libelle = null;
 
