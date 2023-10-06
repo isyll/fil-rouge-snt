fa1e0080f7893e41659be9d76592d02c841ef695
Ibrahima Sylla <isyll711@gmail.com>
2023-10-06 20:05:47 +0000

    création du premier composant

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentLayoutComponent } from './components/content-layout/content-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentLayoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
