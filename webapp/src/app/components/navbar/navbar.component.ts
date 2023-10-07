0d89658ba8587a23723b08208950ffc07d76e13b
Ibrahima Sylla <isyll711@gmail.com>
2023-10-07 13:25:15 +0000

    bouton de recherche

--- webapp/src/app/components/navbar/navbar.component.ts
+++ webapp/src/app/components/navbar/navbar.component.ts
@@ -1,40 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   styleUrls: ['./navbar.component.scss'],
-  template: `
-    <nav class="navbar navbar-dark navbar-expand-lg bg-primary p-2">
-      <div class="container-fluid">
-        <a class="navbar-brand active fs-3" aria-current="page" href="#"
-          >Sylla-App
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link fs-5 disabled" href="#">Cours</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link fs-5" href="#">Absences</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link fs-5" href="#">Élèves</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  `,
+  templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  searchBox!: any;
+  overlay!: any;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.searchBox = this.el.nativeElement.querySelector('.search-box');
+    this.overlay = this.el.nativeElement.querySelector('.overlay');
+  }
+
+  onSearch() {
+    this.renderer.setStyle(this.searchBox, 'display', 'block');
+    this.renderer.setStyle(this.overlay, 'display', 'block');
+  }
+
+  onExit() {
+    this.renderer.setStyle(this.searchBox, 'display', 'none');
+    this.renderer.setStyle(this.overlay, 'display', 'none');
+  }
+}
