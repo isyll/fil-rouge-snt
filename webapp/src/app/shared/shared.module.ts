97d7c956a5bc39ae08957dd1466868577937aa25
Ibrahima Sylla <isyll711@gmail.com>
2023-10-09 15:55:28 +0000

    création composant de chargement

---
+++ webapp/src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './components/loading/loading.component';
+
+@NgModule({
+  declarations: [LoadingComponent],
+  imports: [],
+  exports: [LoadingComponent]
+})
+export class SharedModule {}
