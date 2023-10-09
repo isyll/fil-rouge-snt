d3aa0940e94ff7be60f65ed50bedaa0716acb651
Ibrahima Sylla <isyll711@gmail.com>
2023-10-09 20:32:56 +0000

    intégration composant item dans la liste des classes

--- webapp/src/app/modules/classes/liste/liste.component.ts
+++ webapp/src/app/modules/classes/liste/liste.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ClasseService } from 'src/app/core/openapi';
+import { InfoCardService } from 'src/app/core/services/info-card.service';
 
 @Component({
   selector: 'liste-classes',
@@ -9,15 +10,23 @@ import { ClasseService } from 'src/app/core/openapi';
 export class ClassesComponent implements OnInit {
   data: any;
   requestPending = false;
+  colors!: string[];
 
-  constructor(private classeService: ClasseService) {}
+  constructor(
+    private classeService: ClasseService,
+    private infosCardService: InfoCardService
+  ) {
+    this.colors = this.infosCardService.getColors();
+  }
 
   ngOnInit(): void {
     this.requestPending = true;
 
     this.classeService.apiClassesGetCollection().subscribe((response: any) => {
-      this.data = response;
-      this.requestPending = false;
+      setTimeout(() => {
+        this.data = response;
+        this.requestPending = false;
+      }, 500);
     });
   }
 }
