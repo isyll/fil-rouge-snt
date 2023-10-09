e6296719de4ce60c85883f1422f3b539d0e01871
Ibrahima Sylla <isyll711@gmail.com>
2023-10-09 19:08:05 +0000

    intégration SharedModule

--- webapp/src/app/modules/annees-scolaires/annees-scolaires.module.ts
+++ webapp/src/app/modules/annees-scolaires/annees-scolaires.module.ts
@@ -5,9 +5,15 @@ import { AnneesScolairesComponent } from './liste/liste.component';
 import { AnneeCardComponent } from './annee-card/annee-card.component';
 import { FormComponent } from './form/form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [AnneesScolairesComponent, AnneeCardComponent, FormComponent],
-  imports: [CommonModule, ReactiveFormsModule, AnneesScolairesRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AnneesScolairesRoutingModule,
+    SharedModule,
+  ],
 })
 export class AnneesScolairesModule {}
