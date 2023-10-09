2207ba45bd16de9266f1743d005037f165626981
Ibrahima Sylla <isyll711@gmail.com>
2023-10-09 20:43:37 +0000

    intégration composant item dans niveaux

--- webapp/src/app/modules/niveaux/liste/liste.component.ts
+++ webapp/src/app/modules/niveaux/liste/liste.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NiveauService } from 'src/app/core/openapi';
+import { InfoCardService } from 'src/app/core/services/info-card.service';
 
 @Component({
   selector: 'liste-niveaux',
@@ -9,15 +10,23 @@ import { NiveauService } from 'src/app/core/openapi';
 export class NiveauxComponent {
   data: any;
   requestPending = false;
+  colors!: string[];
 
-  constructor(private niveauService: NiveauService) {}
+  constructor(
+    private niveauService: NiveauService,
+    private infosCardService: InfoCardService
+  ) {
+    this.colors = this.infosCardService.getColors();
+  }
 
   ngOnInit(): void {
     this.requestPending = true;
 
     this.niveauService.apiNiveauxGetCollection().subscribe((response: any) => {
-      this.data = response;
-      this.requestPending = false;
+      setTimeout(() => {
+        this.data = response;
+        this.requestPending = false;
+      }, 500);
     });
   }
 }
