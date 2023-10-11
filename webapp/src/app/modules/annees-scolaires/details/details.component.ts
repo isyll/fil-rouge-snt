379382cc768c0a1fe8283985739beef99cfc38f1
Ibrahima Sylla <isyll711@gmail.com>
2023-10-11 10:41:02 +0000

    recupération des données d'une année scolaire

--- webapp/src/app/modules/annees-scolaires/details/details.component.ts
+++ webapp/src/app/modules/annees-scolaires/details/details.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AnneeScolaireService } from 'src/app/core/openapi';
 
 @Component({
   selector: 'details-annee-scolaire',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent {}
+export class DetailsComponent implements OnInit {
+  data: any;
+  notFound = false;
+  requestPending = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private anneeScolaireService: AnneeScolaireService
+  ) {}
+
+  ngOnInit(): void {
+    this.requestPending = true;
+
+    this.route.params.subscribe((params) => {
+      this.anneeScolaireService
+        .apiAnneeScolairesIdGet(params['id'])
+        .pipe(
+          catchError((error) => {
+            if (error.status !== 0) {
+              this.notFound = true;
+            }
+            this.requestPending = false;
+            return throwError(() => null);
+          })
+        )
+        .subscribe((response: any) => {
+          console.log(response);
+
+          this.data = response;
+          this.requestPending = false;
+        });
+    });
+  }
+}
