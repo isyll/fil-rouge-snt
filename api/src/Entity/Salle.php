58d31bc84d6b0286cc67e777f926b50ea4906b79
Ibrahima Sylla <isyll711@gmail.com>
2023-10-12 12:39:00 +0000

    gestion crud sur Salle

---
+++ api/src/Entity/Salle.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use App\Repository\SalleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: SalleRepository::class)]
+#[UniqueEntity(['nom', 'numero'])]
+#[ApiResource(
+    operations: [new Post, new Get, new GetCollection],
+    normalizationContext: ['groups' => 'read'],
+    denormalizationContext: ['groups' => 'write'],
+)]
+class Salle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    #[Groups(['write', 'read'])]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    #[Groups(['write', 'read'])]
+    private ?string $numero = null;
+
+    #[ORM\Column]
+    #[Groups(['write', 'read'])]
+    private ?int $places = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): static
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getPlaces(): ?int
+    {
+        return $this->places;
+    }
+
+    public function setPlaces(int $places): static
+    {
+        $this->places = $places;
+
+        return $this;
+    }
+}
