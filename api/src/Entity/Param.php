a6e7d7f1ff75972e91204c19b8faa21529125c5b
Ibrahima Sylla <isyll711@gmail.com>
2023-10-13 18:05:49 +0000

    ajout entité Param

---
+++ api/src/Entity/Param.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\ParamRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: ParamRepository::class)]
+#[UniqueEntity('name', message: 'Ce paramètre existe déjà')]
+#[ApiResource(
+    operations: [new GetCollection, new Put, new Post],
+    normalizationContext: ['groups' => 'default'],
+    denormalizationContext: ['groups' => 'default'],
+)]
+#[ApiFilter(SearchFilter::class, properties: ['name' => 'exact'])]
+class Param
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    #[Groups(['default'])]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['default'])]
+    private ?string $value = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups(['default'])]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
