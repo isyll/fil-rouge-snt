bc70e97c18007da0038f942d083e8db86196cd07
Ibrahima Sylla <isyll711@gmail.com>
2023-10-13 00:34:13 +0000

    validation du format de numéro de téléphone

--- api/src/State/InscriptionProcessor.php
+++ api/src/State/InscriptionProcessor.php
@@ -5,6 +5,7 @@ namespace App\State;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Exception\ExistingInscriptionException;
+use App\Exception\PhoneNumberException;
 use App\Repository\InscriptionRepository;
 
 class InscriptionProcessor implements ProcessorInterface
@@ -17,7 +18,10 @@ class InscriptionProcessor implements ProcessorInterface
         $existingInscriptions = $this->inscriptionRepository->findInscription($data);
         if (count($existingInscriptions))
             throw new ExistingInscriptionException("L'élève est déjà inscrit dans cette classe pour cette année.");
+        if (!preg_match('/^\d+$/', $data->getTelephone()))
+            throw new PhoneNumberException('Le numéro de téléphone saisi est invalide');
 
+        $data->setTelephone(str_replace(' ', '', $data->getTelephone()));
         $this->inscriptionRepository->save($data, true);
     }
 }
