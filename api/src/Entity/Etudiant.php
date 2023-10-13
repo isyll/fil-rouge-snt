8c2ffa48e4ff59c5782f5aa5723d85aca34313fa
Ibrahima Sylla <isyll711@gmail.com>
2023-10-13 13:58:33 +0000

    recherche par matricule

--- api/src/Entity/Etudiant.php
+++ api/src/Entity/Etudiant.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
@@ -26,6 +28,7 @@ use Symfony\Component\Validator\Constraints as Assert;
     denormalizationContext: ['groups' => ['write']],
     normalizationContext: ['groups' => ['read']]
 )]
+#[ApiFilter(SearchFilter::class, properties: ['matricule', 'exact'])]
 class Etudiant
 {
     #[ORM\Id]
