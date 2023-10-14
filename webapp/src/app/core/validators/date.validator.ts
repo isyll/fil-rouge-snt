e041c246b96157344690f90e986810f9970db5fd
Ibrahima Sylla <isyll711@gmail.com>
2023-10-14 21:28:29 +0000

    création date validator

---
+++ webapp/src/app/core/validators/date.validator.ts
@@ -0,0 +1,24 @@
+export function dateValidator(control: any) {
+  if (!DATE_REGEXP.test(control.value)) return { invalidDate: true };
+
+  const inputDate = new Date(control.value);
+
+  if (isNaN(inputDate.getTime())) return { invalidDate: true };
+
+  return null;
+}
+
+export function validateTime(time: string): boolean {
+  for (const t of time.split(':')) if (t.match(/^\d+$/) === null) return false;
+
+  const [hours, minutes] = time.split(':').map(Number);
+
+  if (hours >= 24 || hours < 0) return false;
+  if (minutes >= 60 || minutes < 0) return false;
+  console.log(time);
+
+  return true;
+}
+
+export const TIME_REGEXP = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+export const DATE_REGEXP = /^\d{4}\/\d{2}\/\d{2}$/;
--- webapp/src/app/core/validators/index.ts
+++ webapp/src/app/core/validators/index.ts
@@ -1,5 +1,6 @@
-export { anneeScolaireExistenceValidator } from './anneeScolaire-existence.validator';
-export { classeExistenceValidator } from './classe-existence.validator';
-export { nomSalleExistenceValidator } from './salle-existence.validator';
-export { numeroSalleExistenceValidator } from './salle-existence.validator';
-export { moduleExistenceValidator } from './module-existence.validator';
+export * from './anneeScolaire-existence.validator';
+export * from './classe-existence.validator';
+export * from './salle-existence.validator';
+export * from './salle-existence.validator';
+export * from './module-existence.validator';
+export * from './date.validator';
