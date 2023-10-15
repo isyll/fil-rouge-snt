14e43ca44d4f3e7a6992207d2dc1c50904e13045
Ibrahima Sylla <isyll711@gmail.com>
2023-10-15 21:55:54 +0000

    création d'utilisateurs factices

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,41 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Création d'un professeur
+        $prof = new User();
+        $prof->setEmail("user@sa.sn");
+        $prof->setRoles(["ROLE_PROF"]);
+        $prof->setPassword($this->userPasswordHasher->hashPassword($prof, "password"));
+        $manager->persist($prof);
+
+        // Création d'un RP
+        $rp = new User();
+        $rp->setEmail("admin@sa.sn");
+        $rp->setRoles(["ROLE_RP"]);
+        $rp->setPassword($this->userPasswordHasher->hashPassword($rp, "password"));
+        $manager->persist($rp);
+
+        // Création d'un Attaché
+        $attache = new User();
+        $attache->setEmail("attaché@sa.sn");
+        $attache->setRoles(["ROLE_ATTACHE"]);
+        $attache->setPassword($this->userPasswordHasher->hashPassword($attache, "password"));
+        $manager->persist($attache);
 
         $manager->flush();
-    }
+   }
 }
