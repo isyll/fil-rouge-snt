0dfb88daa3890cbcf6c33390a7ab5ed0e2d1b77b
Ibrahima Sylla <isyll711@gmail.com>
2023-10-17 10:00:34 +0000

    FormComponent

--- webapp/src/app/modules/filieres/filieres-routing.module.ts
+++ webapp/src/app/modules/filieres/filieres-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FilieresComponent } from './liste/liste.component';
 import { DetailsComponent } from './details/details.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FilieresComponent,
+  },
+  {
+    path: 'creer-filiere',
+    component: FormComponent,
   },
   {
     path: ':id',
--- webapp/src/app/modules/filieres/filieres.module.ts
+++ webapp/src/app/modules/filieres/filieres.module.ts
@@ -4,9 +4,11 @@ import { FilieresComponent } from './liste/liste.component';
 import { FilieresRoutingModule } from './filieres-routing.module';
 import { DetailsComponent } from './details/details.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormComponent } from './form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [FilieresComponent, DetailsComponent],
-  imports: [CommonModule, FilieresRoutingModule, SharedModule],
+  declarations: [FilieresComponent, DetailsComponent, FormComponent],
+  imports: [CommonModule, FilieresRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class FilieresModule {}
