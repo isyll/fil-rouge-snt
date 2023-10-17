c9d312f3875a3d17025f0a42e3eb0680d153111a
Ibrahima Sylla <isyll711@gmail.com>
2023-10-17 10:02:05 +0000

    FormComponent

--- webapp/src/app/modules/niveaux/niveaux.module.ts
+++ webapp/src/app/modules/niveaux/niveaux.module.ts
@@ -4,9 +4,11 @@ import { NiveauxComponent } from './liste/liste.component';
 import { NiveauxRoutingModule } from './niveaux-routing.module';
 import { DetailsComponent } from './details/details.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormComponent } from './form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [NiveauxComponent, DetailsComponent],
-  imports: [CommonModule, NiveauxRoutingModule, SharedModule],
+  declarations: [NiveauxComponent, DetailsComponent, FormComponent],
+  imports: [CommonModule, NiveauxRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class NiveauxModule {}
