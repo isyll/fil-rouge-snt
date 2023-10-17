be793214be85ac2e9a45f1785834285fef089b5d
Ibrahima Sylla <isyll711@gmail.com>
2023-10-17 09:58:51 +0000

    prise en compte du niveau et de la filière

--- webapp/src/app/modules/classes/form/form.component.ts
+++ webapp/src/app/modules/classes/form/form.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { classeExistenceValidator } from '../../../core/validators';
-import { ClasseService } from '../../../core/openapi';
+import {
+  ClasseService,
+  FiliereService,
+  NiveauService,
+} from '../../../core/openapi';
 import { catchError, throwError } from 'rxjs';
 
 @Component({
@@ -9,7 +13,7 @@ import { catchError, throwError } from 'rxjs';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
   alreadyExists = false;
   submitPending = false;
   submitOk = false;
@@ -20,16 +24,29 @@ export class FormComponent {
       [Validators.required, Validators.minLength(2)],
       [classeExistenceValidator(this.classeService)],
     ],
+    filiere: [null, Validators.required],
+    niveau: [null, Validators.required],
   });
+  filieres: any = null;
+  niveaux: any = null;
 
-  constructor(private fb: FormBuilder, private classeService: ClasseService) {
+  constructor(
+    private fb: FormBuilder,
+    private classeService: ClasseService,
+    private niveauService: NiveauService,
+    private filiereService: FiliereService
+  ) {
     this.form.get('libelle')?.statusChanges.subscribe((status) => {
       const errors = this.form.get('libelle')?.errors;
-      this.alreadyExists = false;
       if (errors && errors['dataNotExist']) {
         this.alreadyExists = true;
-      }
+      } else this.alreadyExists = false;
     });
+  }
+
+  ngOnInit(): void {
+    this.loadNiveaux();
+    this.loadFilieres();
   }
 
   onSubmit() {
@@ -46,11 +63,23 @@ export class FormComponent {
           })
         )
         .subscribe(() => {
-          setTimeout(() => {
-            this.form.reset();
-            this.submitPending = false;
-            this.submitOk = true;
-          }, 500);
+          this.form.reset();
+          this.submitPending = false;
+          this.submitOk = true;
         });
+  }
+
+  private loadNiveaux() {
+    this.niveauService.apiNiveauxGetCollection().subscribe((response: any) => {
+      this.niveaux = response['hydra:member'];
+    });
+  }
+
+  private loadFilieres() {
+    this.filiereService
+      .apiFilieresGetCollection()
+      .subscribe((response: any) => {
+        this.filieres = response['hydra:member'];
+      });
   }
 }
