4ee6699a315d9fea167736b9fb9a8596a767ed82
Ibrahima Sylla <isyll711@gmail.com>
2023-10-17 10:01:42 +0000

    FormComponent

--- webapp/src/app/modules/niveaux/niveaux-routing.module.ts
+++ webapp/src/app/modules/niveaux/niveaux-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NiveauxComponent } from './liste/liste.component';
 import { DetailsComponent } from './details/details.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
   {
     path: '',
     component: NiveauxComponent,
+  },
+  {
+    path:'creer-niveau',
+    component: FormComponent
   },
   {
     path: ':id',
