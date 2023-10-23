759190a8f2c9f0f597597cff2a603cdadceb1934
Ibrahima Sylla <isyll711@gmail.com>
2023-10-23 11:44:43 +0000

    création user lors de création étudiant

--- api/src/State/EtudiantProcessor.php
+++ api/src/State/EtudiantProcessor.php
@@ -4,14 +4,20 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\Entity\User;
 use App\Exception\PhoneNumberException;
 use App\Repository\EtudiantRepository;
+use App\Repository\UserRepository;
 use App\Service\MatriculeGenerator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class EtudiantProcessor implements ProcessorInterface
 {
-    public function __construct(private EtudiantRepository $etudiantRepository)
-    {
+    public function __construct(
+        private EtudiantRepository $etudiantRepository,
+        private UserRepository $userRepository,
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {
     }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
@@ -25,6 +31,12 @@ class EtudiantProcessor implements ProcessorInterface
         } while ($this->etudiantRepository->findOneByMatricule($matricule));
 
         $data->setMatricule($matricule);
+        $this->etudiantRepository->save($data, true);
+        $user = new User;
+        $user->setEmail($data->getEmail());
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+        $user->setRoles(['ROLE_ETUDIANT']);
+        $this->userRepository->save($user);
         $this->etudiantRepository->save($data, true);
     }
 }
